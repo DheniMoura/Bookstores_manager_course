ee56c2f4890d148dc4a09cc21d4f42a561abaed1
DheniMoura <dhenifer@hotmail.com>
2022-02-18 17:41:56 -0300

    First commit: Project structure created

---
+++ src/main/java/com/dhenifer/bookstoresmanager/BookstoresmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.dhenifer.bookstoresmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoresmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoresmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhenifer/bookstoresmanager/BookstoresmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhenifer.bookstoresmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoresmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
